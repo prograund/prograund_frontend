c960510b97eb61ccf1457447114fa041ae046bb0
Humanshu <humanshujaglan@gmail.com>
2024-02-10 10:52:52 +0530

    some changes

--- src/components/sub-components/Host.js
+++ src/components/sub-components/Host.js
@@ -1,7 +1,33 @@
 import React from 'react'
 
 export default function Host() {
+
+  const url = 'http://127.0.0.1:8000/posts/';
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    fetch(url, {
+      method: 'POST',
+      body: data,
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+  }
+
   return (
-    <div>Host</div>
-  )
+    <>
+      <div className="container">
+      <form action="" onSubmit={handleSubmit}>
+        <div className="input-group" style={{overflow:'hidden'}}>
+          <textarea name="post_data" className='form-control' id="" cols="30" rows="10" style={{backgroundColor:'var(--color-2)',color:'var(--color-5)'}}></textarea>
+        </div>
+        <div className="d-grid">
+          <button className="btn btn-primary" type="submit">Send</button>
+        </div>
+      </form>
+      </div>
+    </>
+  );
+
 }
--- src/components/sub-components/dashboard-components/InspectWorld.js
+++ src/components/sub-components/dashboard-components/InspectWorld.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import PostItem from './Post Item/PostItem';
 
 export default function InspectWorld() {
+  const url = 'http://127.0.0.1:8000/posts/';
+
+  const [posts, setPosts] = useState([]);
+
+  const getData = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setPosts(data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
-    <div>InspectWorld</div>
-  )
+    <>
+      {posts.map((item) => (
+        <div className="post" key={item.post_id}>
+          <PostItem title={item.title}/>
+        </div>
+      ))}
+    </>
+  );
 }
+
+
