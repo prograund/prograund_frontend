d3d1c55f1d58f661bb15ac91050a5b1a8c85d466
TheKaushikGoswami <thekaushikgoswami@gmail.com>
2024-02-27 09:36:14 +0530

    Edit: MyProfile.js

--- src/components/sub-components/MyProfile.js
+++ src/components/sub-components/MyProfile.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function MyProfile() {
   return (
-    <div>MyProfile</div>
+    <>
+      <div>
+        <img src='https://upload.canva.com/BAEB9BwkkeQ/MAFyiYj5lYg.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJWF6QO3UH4PAAJ6Q%2F20240226%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240226T070310Z&X-Amz-Expires=36653&X-Amz-Signature=2736303e79cacf47024fca63cd91c62285e436cc28fe2f4924b1ed29aca56a5a&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%2A%3DUTF-8%27%27Parliament-Visit-in-2022.png&response-expires=Mon%2C%2026%20Feb%202024%2017%3A14%3A03%20GMT&version=SnC_KLHvkU.H5_Gez.2fRZhty2Hu6r55' alt='test' style={{width: '100%'}}></img>
+      </div>
+    </>
   )
 }
