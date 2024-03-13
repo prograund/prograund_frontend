b23dda43d1996f814277b350d10f09bb4cb72513
Humanshu <humanshujaglan@gmail.com>
2024-03-13 20:57:02 +0530

    register.js issue done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import './App.css';
-import LAside from './components/LAside';
-import RAside from './components/RAside';
 
 import Configuration from './components/sub-components/Configuration';
 import MyProfile from './components/sub-components/MyProfile';
@@ -22,51 +20,42 @@ import JoySources from './components/sub-components/my-profile/JoySources';
 import Mentions from './components/sub-components/my-profile/Mentions';
 
 import Register from './components/sub-components/Register';
+import Main from './components/Main';
 
 function App() {
   return (
     <>
       <Router>
-        <div className="screen d-flex"> 
-          {/* <Navbar/> */}
-          <LAside />
-          <div className="main col-md-6 p-0" style={{height:'100vh',overflowY:'scroll',overflowX:'hidden'}}>
+
             <Routes>
-              <Route path="/" element={<Dashboard/>}>
-                <Route index element={<DebugStress/>}/>
-              </Route>
-              <Route path="/dashboard" element={<Dashboard/>}>
-                <Route index element={<DebugStress/>}/>
-                <Route path="debug-stress" element={<DebugStress/>}/>
-                <Route path="inspect-world" element={<InspectWorld/>}/>
-              </Route>
-              <Route path="/configuration" element={<Configuration/>}/>
-              <Route path="/my-profile" element={<MyProfile/>}>
-                <Route index element={<Snippets/>}/>
-                <Route path="snippets" element={<Snippets/>}/>
-                <Route path="joy-source" element={<JoySources/>}/>
-                <Route path="mentions" element={<Mentions/>}/>
-              </Route>
-              <Route path="/host" element={<Host/>}/>
-              <Route path="/inbox" element={<Inbox/>}/>
-              <Route path="/tracks" element={<Tracks/>}>
-                <Route index element={<Trackers/>}/>
-                <Route path="trackers" element={<Trackers/>}/>
-                <Route path="trackings" element={<Trackings/>}/>
-              </Route>
-              <Route path="/docs" element={<Docs/>}/>
-              <Route path="*" element={<Dashboard/>}/>
+              <Route path="/" element={<Main/>}>
+                <Route path="/" element={<Dashboard/>}>
+                  <Route index element={<DebugStress/>}/>
+                </Route>
+                <Route path="/dashboard" element={<Dashboard/>}>
+                  <Route index element={<DebugStress/>}/>
+                  <Route path="debug-stress" element={<DebugStress/>}/>
+                  <Route path="inspect-world" element={<InspectWorld/>}/>
+                </Route>
+                <Route path="/configuration" element={<Configuration/>}/>
+                <Route path="/my-profile" element={<MyProfile/>}>
+                  <Route index element={<Snippets/>}/>
+                  <Route path="snippets" element={<Snippets/>}/>
+                  <Route path="joy-source" element={<JoySources/>}/>
+                  <Route path="mentions" element={<Mentions/>}/>
+                </Route>
+                <Route path="/host" element={<Host/>}/>
+                <Route path="/inbox" element={<Inbox/>}/>
+                <Route path="/tracks" element={<Tracks/>}>
+                  <Route index element={<Trackers/>}/>
+                  <Route path="trackers" element={<Trackers/>}/>
+                  <Route path="trackings" element={<Trackings/>}/>
+                </Route>
+                <Route path="/docs" element={<Docs/>}/>
+                <Route path="*" element={<Dashboard/>}/>
+                </Route>
               <Route path="/register" element={<Register/>}/>
             </Routes>
-          </div>
-          <RAside/>
-        </div>
-        <a href="/" className="support-button">
-        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="rgb(117, 79, 229)" className="bi bi-info-circle" viewBox="0 0 16 16">
-        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-        <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
-      </svg>
-        </a>
       </Router>
     </>
   );
--- src/components/sub-components/Register.js
+++ src/components/sub-components/Register.js
@@ -34,20 +34,20 @@ export default function Register() {
             <style
                 dangerouslySetInnerHTML={{
                     __html:
-                        "\n        :root{\n    --color-1: #0f0021;\n    --color-2: #00002c;\n    --color-3: #fcb045;\n    --color-4: #23d18b;\n    --color-5: #29b8db;\n\n    --background-gradient: linear-gradient(to bottom right, var(--color-1), var(--color-2),#0A2E58,#155C84, #1F8AAF, var(--color-5));\n}\n*{\n    font-family: roboto;\n}\n::-webkit-scrollbar{\n    display: none;\n}\nbody{\n    height: 100vh;\n    background: var(--background-gradient);\n}\n.container{\n    display: flex;\n    justify-content: center;\n    align-items: center;\n    height: 100%;\n}\nimg{\n    position: absolute;\n    top: 0;\n    width: 300px;\n}\n.register-card{\n    width: 40rem;\n    background-color: var(--color-2);\n    border-radius: 10px;\n    padding: 20px;\n}\ninput{\n    width: 38%;\n    border-radius: 5px;\n    border: 2px solid white;\n    background-color: transparent;\n    color: white;\n    font-size: 15px;\n    padding: 10px;\n    margin: 10px;\n    \n}\ninput::placeholder{\n    color: rgb(148, 148, 148);\n}\ninput:focus{\n    outline: none;\n}\nh1{\n    color: white;\n    text-align: center;\n    margin: 10px;\n    font-size: 40px;\n}\ninput::-webkit-outer-spin-button,\ninput::-webkit-inner-spin-button {\n  -webkit-appearance: none;\n  margin: 0;\n}\n\ninput[type=submit]{\n    background-color: var(--color-4);\n    color: white;\n    border: none;\n    padding: 10px;\n    border-radius: 5px;\n    margin: 10px;\n    cursor: pointer;\n    width: 15rem;\n}\ninput[type=submit]:hover{\n    background-color: var(--color-3);\n    transition: background-color 0.9s;\n}\nform{\n    width: 100%;\n    display: flex;\n    flex-direction: column;\n    align-items: center;\n}\n.input-group{\n    width: 100%;\n    display: flex;\n    justify-content: center;\n}\n@media screen and (max-width: 600px){\n    .input-group{\n        flex-direction: column;\n        margin: auto;\n    }\n    input{\n        width: 80%;\n        margin: 10px auto;\n    }\n    .register-card{\n        margin-top: 170px;\n    }\n    body{\n        height: unset;\n    }\n}\n    "
+                        "\n        :root{\n    --color-1: #0f0021;\n    --color-2: #00002c;\n    --color-3: #fcb045;\n    --color-4: #23d18b;\n    --color-5: #29b8db;\n\n    --background-gradient: linear-gradient(to bottom right, var(--color-1), var(--color-2),#0A2E58,#155C84, #1F8AAF, var(--color-5));\n}\n*{\n    font-family: roboto;\n}\n::-webkit-scrollbar{\n    display: none;\n}\nbody{\n    height: 100vh;\n    background: var(--background-gradient);\n}\n.container{\n    display: flex;\n    justify-content: center;\n    align-items: center;\n    height: 100%;\n}\nimg{\n    position: absolute;\n    top: 0;\n    width: 300px;\n}\n.register-card{\n    width: 40rem;\n    background-color: var(--color-2);\n    border-radius: 10px;\n    padding: 20px;\n}\ninput{\n    width: 38%;\n    border-radius: 5px;\n    border: 2px solid white;\n    background-color: transparent;\n    color: white;\n    font-size: 15px;\n    padding: 10px;\n    margin: 10px;\n    \n}\ninput::placeholder{\n    color: rgb(148, 148, 148);\n}\ninput:focus{\n    outline: none;\n}\nh1{\n    color: white;\n    text-align: center;\n    margin: 10px;\n    font-size: 40px;\n}\ninput::-webkit-outer-spin-button,\ninput::-webkit-inner-spin-button {\n  -webkit-appearance: none;\n  margin: 0;\n}\n\ninput[type=submit]{\n    background-color: var(--color-4);\n    color: white;\n    border: none;\n    padding: 10px;\n    border-radius: 5px;\n    margin: 10px;\n    cursor: pointer;\n    width: 15rem;\n}\ninput[type=submit]:hover{\n    background-color: var(--color-3);\n    transition: background-color 0.9s;\n}\nform{\n    width: 100%;\n    display: flex;\n    flex-direction: column;\n    align-items: center;\n}\n.input-group-self{\n    width: 100%;\n    display: flex;\n    justify-content: center;\n}\n@media screen and (max-width: 600px){\n    .input-group-self{\n        flex-direction: column;\n        margin: auto;\n    }\n    input{\n        width: 80%;\n        margin: 10px auto;\n    }\n    .register-card{\n        margin-top: 170px;\n    }\n    body{\n        height: unset;\n    }\n}\n    "
                 }}
             />
             <code>
-                <div className="container">
+                <div className="container d-flex justify-content-center align-items-center">
                     <img src="logo-removebg-preview.png" alt="" />
                     <div className="register-card">
                         <h1>Register</h1>
                         <form action="register.php" method="post">
-                            <div className="input-group">
+                            <div className="input-group-self">
                                 <input type="text" name="fname" placeholder="First name" />
                                 <input type="text" name="lname" placeholder="Last name" />
                             </div>
-                            <div className="input-group">
+                            <div className="input-group-self">
                                 <input
                                     type="text"
                                     name="username"
@@ -63,11 +63,11 @@ export default function Register() {
                                     max={9999999999}
                                 />
                             </div>
-                            <div className="input-group">
+                            <div className="input-group-self">
                                 <input type="email" name="email" id="" placeholder="Email" />
                                 <input type="text" name="profession" placeholder="Profession" />
                             </div>
-                            <div className="input-group">
+                            <div className="input-group-self">
                                 <input
                                     type="password"
                                     name="password"
